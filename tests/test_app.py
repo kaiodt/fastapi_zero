0b786e4f384c5117d8e24f3e274a1236d8dc88bb
Kaio Rocha <kaiodtr@gmail.com>
2025-05-15 20:59:04 -0300

    test: move `client` fixture to `conftest.py` file

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,5 @@
 from http import HTTPStatus
 
-import pytest
-from fastapi.testclient import TestClient
-
-from fastapi_zero.app import app
-
-
-@pytest.fixture
-def client():
-    return TestClient(app)
-
 
 def test_root_must_return_hello_world(client):
     response = client.get('/')
