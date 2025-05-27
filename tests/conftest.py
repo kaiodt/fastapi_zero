f127babd3056cd58d3cdf558b4c8c52ab9346aac
Kaio Rocha <kaiodtr@gmail.com>
2025-05-27 14:57:17 -0300

    test: add fixture to get access token for the test user

--- tests/conftest.py
+++ tests/conftest.py
@@ -60,6 +60,19 @@ def user(session):
     return user
 
 
+@pytest.fixture
+def token(client, user):
+    response = client.post(
+        '/token/',
+        data={
+            'username': user.email,
+            'password': user.clean_password,
+        },
+    )
+
+    return response.json()['access_token']
+
+
 @contextmanager
 def _mock_db_time(*, model, time=datetime(2025, 1, 1)):
     def fake_time_hook(mapper, connection, target):
