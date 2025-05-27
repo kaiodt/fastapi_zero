54d6c91a2de37665261872b71df6fd60440b7a48
Kaio Rocha <kaiodtr@gmail.com>
2025-05-27 15:53:28 -0300

    test: add tests for the `get_current_user` function

--- tests/test_security.py
+++ tests/test_security.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from jwt import decode
 
 from fastapi_zero.security import SECRET_KEY, create_access_token
@@ -11,3 +13,41 @@ def test_jwt():
 
     assert decoded['test'] == data['test']
     assert 'exp' in decoded
+
+
+def test_get_current_user_invalid_jwt_token(client, user):
+    response = client.delete(
+        f'/users/{user.id}',
+        headers={'Authorization': 'Bearer invalid-token'},
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Could not validate credentials'}
+
+
+def test_get_current_user_missing_email(client, user):
+    bad_token = create_access_token(
+        data={'no-email': 'test'},
+    )
+
+    response = client.delete(
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {bad_token}'},
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Could not validate credentials'}
+
+
+def test_get_current_user_unexisting_user(client, user):
+    bad_token = create_access_token(
+        data={'sub': 'unknown@test.com'},
+    )
+
+    response = client.delete(
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {bad_token}'},
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Could not validate credentials'}
