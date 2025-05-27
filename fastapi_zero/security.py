74b9e302e14a15bbf49a9b1c1453db952d708c7c
Kaio Rocha <kaiodtr@gmail.com>
2025-05-27 13:43:22 -0300

    feat: add support to password hashing

--- fastapi_zero/security.py
+++ fastapi_zero/security.py
@@ -2,10 +2,13 @@ from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
 
 from jwt import encode
+from pwdlib import PasswordHash
 
 SECRET_KEY = 'your-secret-key'  # Provisory!
 ALGORITHM = 'HS256'
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = PasswordHash.recommended()
 
 
 def create_access_token(data: dict) -> str:
@@ -20,3 +23,11 @@ def create_access_token(data: dict) -> str:
     encoded_jwt = encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
     return encoded_jwt
+
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
