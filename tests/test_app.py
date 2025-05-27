5e923559327e92d1464c0e8efa6ff59c2ab6330b
Kaio Rocha <kaiodtr@gmail.com>
2025-05-27 16:01:23 -0300

    test: add tests for the `/token` `POST` endpoint

--- tests/test_app.py
+++ tests/test_app.py
@@ -25,6 +25,30 @@ def test_get_token(client, user):
     assert 'token_type' in token
 
 
+def test_get_token_for_unexisting_user_should_return_unauthorized(client):
+    response = client.post(
+        '/token/',
+        data={
+            'username': 'unknown@email.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Incorrect email or password'}
+
+
+def test_get_token_wrong_password_should_return_unauthorized(client, user):
+    response = client.post(
+        '/token/',
+        data={
+            'username': user.email,
+            'password': 'wrong_password',
+        },
+    )
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Incorrect email or password'}
+
+
 def test_create_user(client):
     response = client.post(
         '/users/',
